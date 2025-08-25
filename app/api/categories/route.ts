80ce646752ae8558016863a2dcc6a4e652da9357
staffm3 <staffm37@gmail.com>
2025-08-25 21:58:56 +0300

    task fix & done

--- app/api/categories/route.ts
+++
@@ -1,9 +0,0 @@
-export async function GET() {
-	return new Response(
-		JSON.stringify(["All", "Todo", "Work", "Personal", "Meeting", "Shopping"]),
-		{
-			status: 200,
-			headers: { "Content-Type": "application/json" },
-		}
-	)
-}
